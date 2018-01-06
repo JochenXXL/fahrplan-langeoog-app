cadeb0d27c2f0c2c089ab32eec8ff46882aead3c
Sven Harder <info@i-modich.de>
2018-01-06 10:40:12 +0100

    added Container

--- src/screens/BensersielTabScreen.js
+++ src/screens/BensersielTabScreen.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
+import { Text } from 'react-native';
 
-import { View, Text } from 'react-native';
+import Container from '../components/common/Container';
 
 class BensersielTabScreen extends Component {
   render() {
     return (
-      <View>
+      <Container>
         <Text>Hello Bensersiel</Text>
-      </View>
+      </Container>
     );
   }
 }
