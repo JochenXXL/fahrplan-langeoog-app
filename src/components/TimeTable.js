2ade91a82b0060fc5f0d8325167ccd1c382d7927
Sven Harder <info@i-modich.de>
2018-01-06 16:21:15 +0100

    added TimeTableDescription component

--- src/components/TimeTable.js
+++ src/components/TimeTable.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { View, Text, FlatList } from 'react-native';
 
 import ListItem from './common/ListItem';
+import TimeTableDescription from './TimeTableDescription';
 
 const TimeTable = ({ day, location }) => (
   <View>
     <Text>
       Timetable {location} {day}
     </Text>
+    <TimeTableDescription />
     <FlatList
       data={[
         { key: '1', time: '9:00', company: 'SL' },
