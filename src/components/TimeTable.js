15d3adcf9172cac48311663e852afa91b80c2360
Sven Harder <info@i-modich.de>
2018-01-06 18:53:23 +0100

    fetch data from api

--- src/components/TimeTable.js
+++ src/components/TimeTable.js
@@ -1,23 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, FlatList } from 'react-native';
 
 import ListItem from './common/ListItem';
 import TimeTableDescription from './TimeTableDescription';
 
-const data = [
-  { key: '1', time: '07:10', company: 'SL' },
-  { key: '2', time: '09:30', company: 'DW' },
-  { key: '1', time: '09:30', company: 'SL' },
-  { key: '2', time: '11:30', company: 'SL' },
-  { key: '1', time: '16:00', company: 'DW' },
-  { key: '2', time: '16:00', company: 'SL' }
-];
+// const data = [
+//   { key: '1', time: '07:10', company: 'SL' },
+//   { key: '2', time: '09:30', company: 'DW' },
+//   { key: '1', time: '09:30', company: 'SL' },
+//   { key: '2', time: '11:30', company: 'SL' },
+//   { key: '1', time: '16:00', company: 'DW' },
+//   { key: '2', time: '16:00', company: 'SL' }
+// ];
 
-const TimeTable = ({ day, location }) => (
-  <View>
-    <TimeTableDescription />
-    <FlatList data={data} renderItem={({ item }) => <ListItem item={item} />} />
-  </View>
-);
+class TimeTable extends Component {
+  state = {
+    data: []
+  };
+
+  componentWillMount() {
+    fetch('http://fahrplan-langeoog.de/api/test.json')
+      .then(res => res.json())
+      .then(parsedRes => {
+        this.setState({
+          data: parsedRes
+        });
+      });
+  }
+
+  render() {
+    return (
+      <View>
+        <TimeTableDescription />
+        <FlatList
+          data={this.state.data}
+          renderItem={({ item }) => <ListItem item={item} />}
+        />
+      </View>
+    );
+  }
+}
 
 export default TimeTable;
