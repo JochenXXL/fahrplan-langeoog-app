a969f98993d1167445245d1770759a651d1ecbe4
Sven Harder <info@i-modich.de>
2018-01-06 13:17:25 +0100

    added ListItem component

--- src/components/TimeTable.js
+++ src/components/TimeTable.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import { View, Text, FlatList } from 'react-native';
 
+import ListItem from './common/ListItem';
+
 const TimeTable = ({ day, location }) => (
   <View>
     <Text>
       Timetable {location} {day}
     </Text>
     <FlatList
-      data={[{ time: '9:00', company: 'SL' }, { time: '11:00', company: 'DW' }]}
-      renderItem={({ item }) => (
-        <View>
-          <Text>{item.time}</Text>
-          <Text>{item.company}</Text>
-        </View>
-      )}
+      data={[
+        { key: '1', time: '9:00', company: 'SL' },
+        { key: '2', time: '11:00', company: 'DW' }
+      ]}
+      renderItem={({ item }) => <ListItem item={item} />}
     />
   </View>
 );
