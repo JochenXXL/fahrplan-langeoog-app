242382b0b5e1531cdfd015edba94f7d318c1a428
Sven Harder <info@i-modich.de>
2018-01-06 15:52:22 +0100

    added basic styling

--- src/components/common/ListItem.js
+++ src/components/common/ListItem.js
@@ -1,11 +1,37 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 
 const ListItem = ({ item }) => (
-  <View>
-    <Text>{item.time}</Text>
-    <Text>{item.company}</Text>
+  <View style={styles.itemContainerStyle}>
+    <View style={styles.timeContainerStyle}>
+      <Text style={styles.textStyle}>{item.time}</Text>
+    </View>
+    <View style={styles.companyContainerStyle}>
+      <Text style={styles.textStyle}>{item.company}</Text>
+    </View>
   </View>
 );
+
+const styles = StyleSheet.create({
+  itemContainerStyle: {
+    borderColor: '#DCDCDC',
+    borderWidth: 1,
+    flexDirection: 'row',
+    padding: 5,
+    margin: 1,
+    backgroundColor: 'white'
+  },
+  timeContainerStyle: {
+    flex: 3,
+    alignItems: 'center'
+  },
+  companyContainerStyle: {
+    flex: 1,
+    alignItems: 'center'
+  },
+  textStyle: {
+    fontSize: 18
+  }
+});
 
 export default ListItem;
