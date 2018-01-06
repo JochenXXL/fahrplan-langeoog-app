2c7c93d4389ef4e17809bca7d72c5c935aaac03c
Sven Harder <info@i-modich.de>
2018-01-06 13:12:37 +0100

    created DatePicker component

---
+++ src/components/DatePicker.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const DatePicker = ({ location }) => (
+  <View>
+    <Text>DatePicker {location}</Text>
+  </View>
+);
+
+export default DatePicker;
