c2a12d2290ce03238825405eedaaddd4fc4bb8de
Sven Harder <info@i-modich.de>
2018-01-06 10:39:58 +0100

    created Container component

---
+++ src/components/common/Container.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+
+const Container = ({ children }) => (
+  <View style={styles.containerStyle}>{children}</View>
+);
+
+const styles = StyleSheet.create({
+  containerStyle: {
+    padding: 10
+  }
+});
+
+export default Container;
