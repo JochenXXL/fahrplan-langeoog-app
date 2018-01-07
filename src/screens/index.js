9c9910c0f576e12d481afa9de0600d744209fb11
Sven Harder <info@i-modich.de>
2018-01-07 17:38:08 +0100

    removed LangeoogTabScreen and BesnersielTabScreen

--- src/screens/index.js
+++ src/screens/index.js
@@ -1,21 +1,11 @@
 import { Navigation } from 'react-native-navigation';
 
-import BensersielTabScreen from './BensersielTabScreen';
-import LangeoogTabScreen from './LangeoogTabScreen';
 import SettingsTabScreen from './SettingsTabScreen';
 import TimeTableTabScreen from './TimeTableTabScreen';
 import DatePickerTabScreen from './DatePickerTabScreen';
 
 // register main screens
 export function registerScreens() {
-  Navigation.registerComponent(
-    'fpl.BensersielTabScreen',
-    () => BensersielTabScreen
-  );
-  Navigation.registerComponent(
-    'fpl.LangeoogTabScreen',
-    () => LangeoogTabScreen
-  );
   Navigation.registerComponent(
     'fpl.SettingsTabScreen',
     () => SettingsTabScreen
