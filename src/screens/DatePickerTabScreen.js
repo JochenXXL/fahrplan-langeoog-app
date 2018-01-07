693cbc94988bfab710d8f40007827b68524b9aea
Sven Harder <info@i-modich.de>
2018-01-07 17:03:44 +0100

    created DatePickerTabScreen

---
+++ src/screens/DatePickerTabScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Container from '../components/common/Container';
+import DatePicker from '../components/DatePicker';
+
+const DatePickerTabScreen = () => (
+  <Container>
+    <DatePicker location={'not set yet'} />
+  </Container>
+);
+
+export default DatePickerTabScreen;
