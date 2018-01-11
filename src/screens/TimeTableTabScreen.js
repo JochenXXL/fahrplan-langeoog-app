ebfd9f4a3511a32760bd3025c26de935c1018a4b
Sven Harder <info@i-modich.de>
2018-01-11 11:29:12 +0100

    removed Container and Description

--- src/screens/TimeTableTabScreen.js
+++ src/screens/TimeTableTabScreen.js
@@ -2,19 +2,16 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 
-import Container from '../components/common/Container';
+import TimeTableDate from '../components/TimeTableDate';
 import TimeTable from '../components/TimeTable';
-import TimeTableDescription from '../components/TimeTableDescription';
 
 class TimeTableTabScreen extends Component {
   render() {
     console.log('loaded timetable');
     return (
       <View>
-        <Container>
-          <TimeTableDescription />
-          <TimeTable date={this.props.date} location="Bensersiel" />
-        </Container>
+        <TimeTableDate date={'11. Januar 2018'} />
+        <TimeTable date={this.props.date} location="Bensersiel" />
       </View>
     );
   }
