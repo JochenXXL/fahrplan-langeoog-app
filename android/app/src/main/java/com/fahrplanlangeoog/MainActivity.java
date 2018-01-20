5a652c0e00fd6147d5ca9b32f8a3b9a9335c9cc5
Sven Harder <info@i-modich.de>
2018-01-20 19:15:43 +0100

    integrated react-native-splash-screen

--- android/app/src/main/java/com/fahrplanlangeoog/MainActivity.java
+++ android/app/src/main/java/com/fahrplanlangeoog/MainActivity.java
@@ -1,7 +1,13 @@
 package com.fahrplanlangeoog;
 
+import android.os.Bundle;
 import com.reactnativenavigation.controllers.SplashActivity;
+import org.devio.rn.splashscreen.SplashScreen;
 
 public class MainActivity extends SplashActivity {
-
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+      SplashScreen.show(this);  // here
+      super.onCreate(savedInstanceState);
+  }
 }
