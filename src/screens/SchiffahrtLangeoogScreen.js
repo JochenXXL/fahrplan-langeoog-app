aa244cde06ab93f2820534cfa95ac06560d8ab16
Sven Harder <info@i-modich.de>
2018-01-21 10:46:11 +0100

    created ferry info screens

---
+++ src/screens/SchiffahrtLangeoogScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Text, View } from 'react-native';
+
+const SchiffartLangeoogScreen = () => (
+  <View style={{ backgroundColor: 'white', flex: 1 }}>
+    <Text>Info über die Schiffahrt Langeoog</Text>
+  </View>
+);
+
+export default SchiffartLangeoogScreen;
--- src/screens/index.js
+++ src/screens/index.js
@@ -3,6 +3,8 @@ import { Navigation } from 'react-native-navigation';
 import SettingsTabScreen from './SettingsTabScreen';
 import TimeTableTabScreen from './TimeTableTabScreen';
 import DatePickerTabScreen from './DatePickerTabScreen';
+import SchiffahrtLangeoogScreen from './SchiffahrtLangeoogScreen';
+import MSFlinthoernScreen from './MSFlinthoernScreen';
 
 // register main screens
 export function registerScreens() {
@@ -17,5 +19,13 @@ export function registerScreens() {
   Navigation.registerComponent(
     'fpl.DatePickerTabScreen',
     () => DatePickerTabScreen
+  );
+  Navigation.registerComponent(
+    'fpl.SchiffahrtLangeoogScreen',
+    () => SchiffahrtLangeoogScreen
+  );
+  Navigation.registerComponent(
+    'fpl.MSFlinthoernScreen',
+    () => MSFlinthoernScreen
   );
 }
