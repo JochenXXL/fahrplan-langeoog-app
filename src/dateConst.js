defeb60945e6475df05cc92369ce5b57decf887b
Sven Harder <info@i-modich.de>
2018-01-22 18:34:10 +0100

    added german WEEKDAY and MONTH names

---
+++ src/dateConst.js
@@ -0,0 +1,24 @@
+export const WEEKDAY = {
+  0: 'Sonnatg',
+  1: 'Montag',
+  2: 'Dienstag',
+  3: 'Mittwoch',
+  4: 'Donnerstag',
+  5: 'Freitag',
+  6: 'Samstag'
+};
+
+export const MONTH = {
+  0: 'Januar',
+  1: 'Februar',
+  2: 'März',
+  3: 'April',
+  4: 'Mai',
+  5: 'Juni',
+  6: 'Juli',
+  7: 'August',
+  8: 'September',
+  9: 'Oktober',
+  10: 'November',
+  11: 'Dezember'
+};
