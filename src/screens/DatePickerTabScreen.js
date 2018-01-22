4693567e6c5e289d729d03e535e1c9b95f03f9f7
Sven Harder <info@i-modich.de>
2018-01-22 19:11:22 +0100

    added navigator to props

--- src/screens/DatePickerTabScreen.js
+++ src/screens/DatePickerTabScreen.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import DatePicker from '../components/DatePicker';
 
-const DatePickerTabScreen = () => <DatePicker location={'not set yet'} />;
+const DatePickerTabScreen = ({ location, navigator }) => (
+  <DatePicker location={location} navigator={navigator} />
+);
 
 export default DatePickerTabScreen;
